6c6547c24be272e07b71ed1280eea710883c5d3d
Okui Sora <241110@std.hi-joho.ac.jp>
2025-05-14 10:03:25 +0900

    Githubにアップロードする手順をデモするためにファイルを変更した

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP ハイライトされたテキストにキャレットがある状態で <shortcut actionId="ShowIntentionActions"/> を押すと
         // IntelliJ IDEA によるその修正案を確認できます。
-        System.out.print("奥井 颯天");
+        System.out.print("奥井 颯天 {おくい そら}");
 
         for (int i = 1; i <= 5; i++) {
             //TIP <shortcut actionId="Debug"/> を押してコードのデバッグを開始します。<icon src="AllIcons.Debugger.Db_set_breakpoint"/> ブレークポイントを 1 つ設定しましたが、
